d6b9b55866751c23d0280ecc1e96a55c633d0197
zeinab <z.sedaaghat@gmail.com>
2022-05-07 02:19:29 +0430

    init mass

---
+++ Mass2/AccountConsumer.cs
@@ -0,0 +1,17 @@
+﻿using MassTransit;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mass2
+{
+    public class AccountConsumer : IConsumer<UpdateAccount>
+    {
+        public async Task Consume(ConsumeContext<UpdateAccount> context)
+        {
+            Console.WriteLine($"vaaaaaaaaaaay{context.Message.AccountNumber}");
+        }
+    }
+}
---
+++ Mass2/Program.cs
@@ -0,0 +1,46 @@
+﻿using MassTransit;
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Mass2
+{
+    public class Program
+    {
+        static async Task Main(string[] args)
+        {
+            var busController = Bus.Factory.CreateUsingRabbitMq(cfq =>
+           {
+               cfq.Host("localhost", h =>
+               {
+                   h.Username("guest");
+                   h.Password("guest");
+               });
+               cfq.ReceiveEndpoint("AccountService", e =>
+               {
+                   e.Lazy = true;
+                   e.PrefetchCount = 20;
+                   e.Consumer<AccountConsumer>();
+               }
+              );
+
+
+           });
+
+            using var cancelation = new CancellationTokenSource(TimeSpan.FromSeconds(30));
+            await busController.StartAsync(cancelation.Token);
+            try
+            {
+                await busController.Publish<UpdateAccount>(new
+                {
+                    AccountNumber = "123iiii"
+                });
+            }
+            finally
+            {
+                await busController.StopAsync(cancelation.Token);
+            }
+
+        }
+    }
+}
